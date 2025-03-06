16c1910f8e1213bfd1d3da67a7794e1e43e93303
Abdur Razzaq <abdurrazaq789@gmail.com>
2025-03-07 03:06:44 +0500

    lecture 78 | Full Course Completed

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,14 +1,19 @@
 import React from 'react'
-
+import { useDispatch, useSelector} from 'react-redux'
+import { bindActionCreators } from 'redux';
+import {actionCreators} from '../State/index'
 const Shop = () => {
+    const dispatch =  useDispatch();
+    const {depositMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch);
+    const amount = useSelector(state=> state.amount)
   return (
     <>
     <h1 className='text-center text-3xl mx-4 mt-4'>Deposite/Widthdraw Money</h1>
     <div className='flex justify-center items-center my-10'>
         
-      <button className="btn btn-primary mx-2">-</button>
-      Update Balance
-      <button className="btn btn-primary mx-2">+</button>
+      <button onClick={()=>{withdrawMoney(15000)}} className="btn btn-primary mx-2">-</button>
+      Update Balance ({amount})
+      <button onClick={()=>{depositMoney(15000)}} className="btn btn-primary mx-2">+</button>
     </div>
     </>
   )
